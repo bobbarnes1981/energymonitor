850447998b2274731a6c80f256a23f3e74d9d8c5
Robert Barnes <robertbarnes1981@googlemail.com>
2022-04-26 13:22:51 +0100

    Remove delay in startup

--- software/__main__.py
+++ software/__main__.py
@@ -5,10 +5,7 @@ import time
 logging.basicConfig(level=logging.INFO)
 
 logging.info('Connecting')
-with serial.Serial('/dev/ttyUSB0', 9600, timeout=2) as s:
-  logging.info('Waiting 3 seconds...')
-  time.sleep(3)
-
+with serial.Serial('/dev/ttyUSB0', 9600, timeout=2, dsrdtr=True) as s:
   logging.info('Connection: ' + s.name)
 
   while True:
